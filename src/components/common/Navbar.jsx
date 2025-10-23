a5c993d3c58d3bdf7b3fbaf9e77f72e53abcbbdf
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-10-23 21:21:23 +0530

    updated menu

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,13 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Navbar = ({ sections }) => {
+  const [isActive, setIsActive] = useState(true);
+
   const handleScroll = (ref) => {
     if (ref?.current) {
       ref.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+  useEffect(() => {
+    const handleScrollPosition = () => {
+      if (window.scrollY > window.innerHeight) {
+        setIsActive(false);
+      } else {
+        setIsActive(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScrollPosition);
+    return () => window.removeEventListener("scroll", handleScrollPosition);
+  }, []);
 
   return (
     <nav>
@@ -19,7 +34,8 @@ const Navbar = ({ sections }) => {
           alt="aka-logo"
         />
       </Link>
-      <div id="menu_btn">
+
+      <div id="menu_btn" className={isActive ? "active" : ""}>
         <div onClick={() => handleScroll(sections.home)}>
           <span>
             Menu <span id="home">Home</span>
