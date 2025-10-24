6aa343dc259aff443aaff88fed1087247d8bec9a
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-10-24 12:50:24 +0530

    updated animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+"use client"
+
 import Footer from '@/components/common/Footer'
 import Navbar from '@/components/common/Navbar'
 import AboutSection from '@/components/home/AboutSection'
@@ -6,18 +8,52 @@ import HeroSection from '@/components/home/HeroSection'
 import ShowcaseSection from '@/components/home/ShowcaseSection'
 import StorySection from '@/components/home/StorySection'
 import WhySection from '@/components/home/WhySection'
+import Image from 'next/image'
+import React, { useRef, useEffect } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 
-import React, { useRef } from 'react'
+gsap.registerPlugin(ScrollTrigger)
 
 const Home = () => {
   const homeRef = useRef(null)
   const aboutRef = useRef(null)
   const gamesRef = useRef(null)
   const skinRef = useRef(null)
+  const astronautRef = useRef(null)
+
+  useEffect(() => {
+    if (astronautRef.current && aboutRef.current) {
+      gsap.to(astronautRef.current, {
+        scrollTrigger: {
+          trigger: aboutRef.current,
+          start: "top bottom", // when top of AboutSection hits center of viewport
+          end: "bottom center",
+          scrub: true, // smooth animation
+
+        },
+        top: "27%", // new position
+        left: "45%", // center horizontally
+        scale:1.2,
+        onComplete: () => {
+          // Change the image src right when animation starts
+          astronautRef.current.src = "/images/astronaut2.png";
+        }
+      })
+    }
+  }, [])
 
   return (
     <main>
       <Navbar sections={{ home: homeRef, about: aboutRef, games: gamesRef, skin: skinRef }} />
+
+      <img
+        ref={astronautRef}
+        id='astronaut'
+        src="/images/astronaut.png"
+        alt='astronaut'
+      />
+
       <div ref={homeRef}>
         <HeroSection />
       </div>
@@ -27,6 +63,7 @@ const Home = () => {
       </div>
 
       <WhySection />
+
       <div ref={gamesRef}>
         <ShowcaseSection />
       </div>
@@ -34,6 +71,7 @@ const Home = () => {
       <div ref={skinRef}>
         <ConqureSection />
       </div>
+
       <StorySection />
       <Footer />
     </main>
