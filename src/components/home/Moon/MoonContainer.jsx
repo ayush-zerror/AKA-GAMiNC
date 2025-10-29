5f5885e9def328b74f9ff3a50f004b6166c124a6
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-10-29 17:01:51 +0530

    updated hero

--- src/components/home/Moon/MoonContainer.jsx
+++ src/components/home/Moon/MoonContainer.jsx
@@ -6,17 +6,35 @@ import React from "react";
 const MoonContainer = () => {
   return (
     <div id="moon_container">
-      <Image
-        id="white-moon"
-        width={400}
-        height={400}
-        src="/images/hero-moon.webp"
-        alt="white-moon"
-      />
+      <div id="moon_wrap">
         <Image
+          id="white-moon"
+          width={1000}
+          height={1000}
+          src="/images/hero-moon.webp"
+          alt="white-moon"
+        />
+        <div id="platform_container">
+           <Image
+          id="platform"
+          width={1000}
+          height={1000}
+          src="/images/platform.svg"
+          alt="platform"
+        />
+          <Image
+          id="astronaut"
+          width={1000}
+          height={1000}
+          src="/images/astronaut.svg"
+          alt="astronaut"
+        />
+        </div>
+      </div>
+      <Image
         id="spaceship"
-        width={400}
-        height={400}
+        width={1000}
+        height={1000}
         src="/images/spaceship.svg"
         alt="spaceship"
       />
